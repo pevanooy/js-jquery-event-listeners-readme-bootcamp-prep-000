57b315cb2f0a7578fa8164b8df416d59ee821768
Peter van Ooy <pevanooy@gmail.com>
2017-10-04 19:06:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,3 +21,8 @@ function pressIt(){
     if (key.which == 71) alert("You pressed the G key!")
   })
 }
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
