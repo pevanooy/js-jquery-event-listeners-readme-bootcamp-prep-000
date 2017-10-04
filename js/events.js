57d5790a14a789fd1c0a92528ab91a30f8b14e95
Peter van Ooy <pevanooy@gmail.com>
2017-10-04 19:01:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
 });
 
 function getIt(){
-
   $('p').on('click', function() {
       alert('Hey!');
   });
-
+}
+function frameIt(){
+  $('document').on('load', function(){
+    $('img').addClass('tasty')
+  })
 }
