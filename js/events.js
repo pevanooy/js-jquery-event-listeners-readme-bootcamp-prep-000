d442a8f200ab04d84f8e63553433ac640ff72dcb
Peter van Ooy <pevanooy@gmail.com>
2017-10-04 19:07:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  $('p').on('click', function() {
+      alert('Hey!');
+  });
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('input').on('keydown',function(key){
+    if (key.which == 71) alert("You pressed the G key!")
+  })
+}
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
